362e1372c6727f834d1f3bd41672836f474f05c0
whwWu <784584244@qq.com>
2017-05-05 00:37:06 +0800

    提交文件

---
+++ js/unicorn.form_validation.js
@@ -0,0 +1,224 @@
+/**
+ * Unicorn Admin Template
+ * Diablo9983 -> diablo9983@gmail.com
+**/
+$(document).ready(function(){
+	
+	$('input[type=checkbox],input[type=radio],input[type=file]').uniform();
+	
+	$('select').select2();
+	
+	// Form Validation
+    $("#basic_validate").validate({
+		rules:{
+			required:{
+				required:true
+			},
+			email:{
+				required:true,
+				email: true
+			},
+			date:{
+				required:true,
+				date: true
+			},
+			url:{
+				required:true,
+				url: true
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+
+	/*新增类型的js校验*/
+	$("#newstype_reg_validate").validate({
+		rules:{
+			typeName:{
+				required:true
+				
+			},
+			typeRemark:{
+				required:true
+
+			}
+			
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+
+
+	/*新增新闻的js校验*/
+	$("#basic_addNews_validate").validate({
+		rules:{
+			newsType:{
+				required:true
+				
+			},
+			newsTitle:{
+				required:true
+
+			},
+			newsContent:{
+				required:true
+
+			},
+			newsRemark:{
+				required:true
+
+			},
+			
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+
+
+
+	/*新增商品的js校验*/
+	/*basic_addGoods_validate 表单的ID*/
+	$("#basic_addGoods_validate").validate({
+		rules:{
+			goodsType:{
+				required:true
+				
+			},
+			goodsName:{
+				required:true
+
+			},
+			goodsContent:{
+				required:true
+
+			},
+			
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+
+
+	$("#basic_reg_validate").validate({
+		rules:{
+			required:{
+				required:true
+			},
+			email:{
+				required:true,
+				email: true
+			},
+			date:{
+				required:true,
+				date: true
+			},
+			url:{
+				required:true,
+				url: true
+			},
+			pwd:{
+				required: true,
+				minlength:6,
+				maxlength:20
+			},
+			pwd2:{
+				required:true,
+				minlength:6,
+				maxlength:20,
+				equalTo:"#pwd"
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+
+
+
+
+
+	$("#number_validate").validate({
+		rules:{
+			min:{
+				required: true,
+				min:10
+			},
+			max:{
+				required:true,
+				max:24
+			},
+			number:{
+				required:true,
+				number:true
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+	
+	$("#password_validate").validate({
+		rules:{
+			pwd:{
+				required: true,
+				minlength:6,
+				maxlength:20
+			},
+			pwd2:{
+				required:true,
+				minlength:6,
+				maxlength:20,
+				equalTo:"#pwd"
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+});
